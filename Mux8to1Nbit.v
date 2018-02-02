c6cdc8a882e05f066deac6abf5512169a93bc46b
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-02-02 14:21:42 -0500

    Update Mux8to1Nbit.v
    Made "Module" into "module"

--- Mux8to1Nbit.v
+++ Mux8to1Nbit.v
@@ -1,4 +1,4 @@
-Module Mux8to1Nbit (F, S, I0, I1, I2, I3, I4, I5, I6, I7);
+module Mux8to1Nbit (F, S, I0, I1, I2, I3, I4, I5, I6, I7);
 	parameter N = 64;
 	input [N-1:0] I0, I1, I2, I3, I4, I5, I6, I7;
 	input [3:0] S;
