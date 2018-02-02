8c5363557769095878788876b48eb101d993b5c4
sheppardd1 <35700049+sheppardd1@users.noreply.github.com>
2018-02-02 14:24:07 -0500

    Update Mux8to1Nbit.v
    corrected assign F line

--- Mux8to1Nbit.v
+++ Mux8to1Nbit.v
@@ -4,6 +4,6 @@ module Mux8to1Nbit (F, S, I0, I1, I2, I3, I4, I5, I6, I7);
 	input [3:0] S;
 	output [N-1] F;
 	
-	assign F = S[2]? (S[1] (S[0]? I7:I6):(S[0]?I5:I4)):(S[1]? (S[0]? I3:I2): (S[0]? I1:I0);
+	assign F = S[2] ? (S[1] ? (S[0] ? I7:I6):(S[0] ? I5:I4)) : (S[1] ? (S[0] ? I3:I2):(S[0] ? I1:I0));
 	
 	endmodule
