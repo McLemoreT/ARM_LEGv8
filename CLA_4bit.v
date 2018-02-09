5f6329225b58b1298acfd36a4378769df475d3c9
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-02-09 13:37:09 -0500

    Removed PG and GG from CLA_4bit

--- CLA_4bit.v
+++ CLA_4bit.v
@@ -14,6 +14,5 @@ module CLA_4bit(
     assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
     assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) |(P[3] & P[2] & P[1] & P[0] & C[0]);
     assign S = P ^ C;
-    assign PG = P[3] & P[2] & P[1] & P[0];
-    assign GG = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+
 endmodule
