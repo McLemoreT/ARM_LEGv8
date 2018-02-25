903a1b1c04c55f463671529519d67001fc47f417
sheppardd1 <35700049+sheppardd1@users.noreply.github.com>
2018-02-24 20:46:34 -0500

    Add files via upload

--- DatapathLEGv8.v
+++ DatapathLEGv8.v
@@ -23,9 +23,12 @@ module DatapathLEGv8(ControlWord, status, constant, data, clock, reset);
 	
 	//RAM256x64sim data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
 	//RAM256x64m9k data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
-	RAM256x64 data_mem (ALU_output, ~clock, RegBbus, MemWrite, MEM_output);
+	//RAM256x64 data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
 	
-	
+	//RAM256x64 data_mem (ALU_output, ~clock, RegBbus, MemWrite, MEM_output); 
+		//clock is inverted so ALU operations happen before reading values. Gives time for fan-in time
+	RAM256x64 data_mem (ALU_output, ~clock, B, MemWrite, MEM_output);
+
 	//defparam data_mem.memory_words = 7000;
 	
 	assign data = EN_Mem ? MEM_output : 64'bz;
--- Datapath_testbench.v
+++ Datapath_testbench.v
@@ -1,47 +1,55 @@
 module Datapath_testbench (); 
 			
-/*	reg [63:0] A, B; //2 main inputs for ALU
-	wire [63:0]F; //answer output of ALU
-	wire [3:0]status; //status signalfrom ALU
-	reg [4:0]FS;	//Ainvert (FS[0]), Binvert (FS[1]), and function selector for ALU (FS[4:2])
-		//FS[4:2] funcitons are: 000 AND, 001 OR, 010 ADD, 011 xor, 100 shift left, 101 shift right	
-		//note that when FS[4:0] = 11000 and beyond, output should be all zeroes, because it's undefined
-	reg CO; //carry in?*/
-
-wire [63:0] R5, R2, R0, R30, R15, R12, R1, /*M5,*/ R28, /*M200,*/ R22;
+wire [63:0] R5, R2, R0, R30, R15, R12, R1, R28, R22;
 wire [63:0] data;
 reg [63:0] constant;
 reg [24:0] ControlWord;
 reg clock, reset;
 wire [3:0] status;
-reg [63:0] D, DA, W;
-
+wire [63:0] M5, M200;
 
 	initial begin
 		constant <= 64'd0;
 		ControlWord <= 25'b0;
 		reset <= 1;
 		clock <= 0;
-		#120 $stop; //then stop
+		#165 $stop; //then stop
 	end
 //Control word is defined as: {SA, SB, DA, RegWrite, MemWrite, FS, Bsel, EN_Mem, EN_ALU} = ControlWord;
 	always begin
-		//#10 data <= 64'd256;
-		//ControlWord <= 25'b0000100010000001000000000;
 		#5 reset <= 0;
-		#15 constant <= 64'd4;
-		ControlWord <= 25'b1111100000001011001000101;
-		#18 D <= 64'd6;	//perform preconditoin of laoding 6 into R2
-		W <= 1;
-		DA <= 5'd2;
-		#1 D <= 64'd35;	//loading 35 into R0
-		DA <= 5'd0;
-		#1 ControlWord <= 25'b0001000000001011001000001;
-		#20 ControlWord <= 25'b0111101100111101001001001;
-		#20 constant <= 64'b0;
-		ControlWord <= 25'b0000100010000000101000110;
-		#20 ControlWord <= 25'b1011000000111001001000110;
-		//#20 ControlWord <= 25'b0000000000000000000000000;
+		#5 constant <= 64'd4;
+		ControlWord <= 25'b1111100000001011001000101;//loading 4 into R5
+		
+		#10 constant <= 64'd6;
+		ControlWord <= 25'b1111100000000101001000101; //loading 6 into r2
+		#10 constant <= 64'd35;
+		ControlWord <= 25'b1111100000000001001000101; // loading 35 into R0
+		
+		#10 ControlWord <= 25'b0001000000001011001000001; //adding R2+R0 and putting in R5
+		
+		#10 constant <= 64'd34;
+		ControlWord <= 25'b1111100000011111001000101; // loading 34 into R15
+		#10 constant <= 64'd30;
+		ControlWord <= 25'b1111100000011001001000101; // loading 30 into R12
+		#10 ControlWord <= 25'b0111101100111101001010001; //subtracting R15-R12, goes to R30
+		//------------------------------------
+
+		#10 constant <= 64'd5;
+		ControlWord <= 25'b1111100000000011001000101; // loading 5 into R1
+		#10 constant <= 64'd7;
+		ControlWord <= 25'b1111100000000101001000101; // loading 7 into R2
+		#10 constant <= 64'd0;
+		ControlWord <= 25'b0000100010000000101000110; //M5 gets R2, (5 is the memory address, stored in R1)
+		
+		#10 constant <= 64'd200;
+		ControlWord <= 25'b1111100000101101001000101; // loading 200 into R22
+		#10 constant <= 64'd18;
+		ControlWord <= 25'b1111100000000001001000101; // loading 18 into R0 (arbitrary register)
+		#10 constant <= 64'd0;
+		ControlWord <= 25'b1011000000000000101000110; //M200 gets 18 (18 was stored in R28)
+		#10 ControlWord <= 25'b1011000000111001001000110; //R28 get value 18 which is stored in M200
+		#20 reset <= 1;
 	end
 	
 	always begin
@@ -49,18 +57,20 @@ reg [63:0] D, DA, W;
 	end
 	
 	DatapathLEGv8 dut (ControlWord, status, constant, data, clock, reset);	
-	RegFile32x64 dutReg (A, B, D, DA, SA, SB, W, reset, clock);
-	
+
+	//viewing the necessary register locations in Modelsim
+	assign R0 = dut.regfile.R00;	
+	assign R1 = dut.regfile.R01;	
+	assign R2 = dut.regfile.R02;	
 	assign R5 = dut.regfile.R05;
-	assign R2 = dut.regfile.R02;
-	assign R0 = dut.regfile.R00;
-	assign R30 = dut.regfile.R30;
-	assign R15 = dut.regfile.R15;
 	assign R12 = dut.regfile.R12;
-	assign R1 = dut.regfile.R01;
-	assign R28 = dut.regfile.R28;
+	assign R15 = dut.regfile.R15;
 	assign R22 = dut.regfile.R22;
+	assign R28 = dut.regfile.R28;
+	assign R30 = dut.regfile.R30;	
 
-
+	//viewing the necessary memory locations in Modelsim
+	assign M5 = dut.data_mem.mem[5];
+	assign M200 = dut.data_mem.mem[200];
 
 endmodule
