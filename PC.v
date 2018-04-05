9669e2fef5ca32da4cf67a007eb318e6a05c43a5
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-04-05 10:11:32 -0400

    Added PC (Program Counter)

--- PC.v
+++ PC.v
@@ -1,48 +1,23 @@
-module PC (clock, load, reset, PS, X, PC_out); 
-	input clock, load, reset;
-	input [1:0]PS;
+module ProgramCounter(PC, PC4, in, PS, clock, reset);
+	input [63:0]in;
+	input [1:0]PS; // Program counter function Select bits
+	// PS
+	// 00 - PC <= PC
+	// 01 - PC <= PC+4
+	// 10 - PC <= in
+	// 11 - PC <= PC+4+in*4
+	input clock, reset;
+	output [63:0]PC;
+	output [63:0]PC4;
 	
-	input [63:0] X;
-	output [63:0]PC_out;
-	
-	//wire [63:0] PC4;
-	reg [63:0] PC4;
-	wire [63:0] PC_out; 
-	wire [63:0]PC_in;	
-	
-	reg [63:0]Adder_Out;
-
-	
-	wire [63:0]mux1Out;
-	
-
-
-
-	
-	RegisterNbit Register(PC_out, PC_in, load, reset, clock);
-	/*
-	initial begin
-		X <= 64'd10;
-	end
-	*/
-	
-	
-	always @ (posedge clock) begin
-		PC4 <= PC_out + 4;
-		
-		//PC_in <= mux1Out;
-	end
-	
-	
-	always @ (posedge clock) begin
-		Adder_Out <= PC4 + X;
-	end
-	
-	assign PC_in = PS[1] ? Adder_Out : PC4;	
-	
-	
-
-
-	
-	
+	wire [63:0] PC_in; // input to program counter / output from mux
+	wire [63:0] AdderOut; // output of offeset adder
+	wire load_PC;
+	assign load_PC = 1'b1;
+	assign PC4 = PC + 4; // +4 adder
+	assign AdderOut = PC4 + {in[63:1], 2'b00};
+	Mux4to1Nbit PC_Mux (PC_in, PS, PC, PC4, in, AdderOut);
+	defparam PC_Mux.N = 64;
+	RegisterNbit PC_Reg (PC, PC_in, load_PC, reset, clock);
+	defparam PC_Reg.N = 64;
 endmodule
