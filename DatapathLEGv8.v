13436bc4543a040a70b5447c194babb2bf76d15e
sheppardd1 <35700049+sheppardd1@users.noreply.github.com>
2018-04-12 12:04:01 -0400

    Updated with extra status Reg

--- DatapathLEGv8.v
+++ DatapathLEGv8.v
@@ -3,7 +3,7 @@ module DatapathLEGv8(ControlWord, status, constant, data, clock, reset);
 	input [63:0] constant;
 	inout [63:0] data;
 	input clock, reset;
-	output [3:0] status;
+	output [4:0] status;
 	
 	wire [4:0] SA, SB, DA;
 	wire RegWrite, MemWrite;
@@ -12,6 +12,7 @@ module DatapathLEGv8(ControlWord, status, constant, data, clock, reset);
 	wire [63:0] ALU_output, MEM_output;
 	wire EN_Mem, EN_ALU;
 	wire Bsel;
+	wire [3:0] ALU_Status;
 	
 	assign {SA, SB, DA, RegWrite, MemWrite, FS, Bsel, EN_Mem, EN_ALU} = ControlWord;
 	
@@ -19,13 +20,21 @@ module DatapathLEGv8(ControlWord, status, constant, data, clock, reset);
 	
 	assign RegBbus = Bsel ? constant : B;
 	
-	ALU_LEGv8 alu (RegAbus, RegBbus, FS, FS[0], ALU_output, status);
+	ALU_LEGv8 alu (RegAbus, RegBbus, FS, FS[0], ALU_output, ALU_status);
+	
+	RegisterNbit statusReg (status[4:1], ALU_Status, SL, reset, clock); //SL is part of control work. need to figure out how to get that here
+	defparam statusReg.N = 4;
+	
+	assign status[0] = ALU_Status[0];
 	
 	//RAM256x64sim data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
 	//RAM256x64m9k data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
-	RAM256x64 data_mem (ALU_output, ~clock, RegBbus, MemWrite, MEM_output);
+	//RAM256x64 data_mem (ALU_output, clock, RegBbus, MemWrite, MEM_output);
 	
-	
+	//RAM256x64 data_mem (ALU_output, ~clock, RegBbus, MemWrite, MEM_output); 
+		//clock is inverted so ALU operations happen before reading values. Gives time for fan-in time
+	RAM256x64 data_mem (ALU_output, ~clock, B, MemWrite, MEM_output);
+
 	//defparam data_mem.memory_words = 7000;
 	
 	assign data = EN_Mem ? MEM_output : 64'bz;
