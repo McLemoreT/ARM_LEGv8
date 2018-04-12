cc72d97a1ebba12b541f90bc5455dfa947429e11
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-04-12 10:12:56 -0400

    Fixed CLA_4bit

--- CLA_4bit.v
+++ CLA_4bit.v
@@ -1,6 +1,6 @@
 module CLA_4bit(
     output [3:0] S,
-    output Cout,
+    output PG,GG,
     input [3:0] A,B,
     input Cin
     );
@@ -14,5 +14,6 @@ module CLA_4bit(
     assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
     assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) |(P[3] & P[2] & P[1] & P[0] & C[0]);
     assign S = P ^ C;
-
+    assign PG = P[3] & P[2] & P[1] & P[0];
+    assign GG = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
 endmodule
