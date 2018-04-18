5dadd153167667c6b1587ad32ab7524b11501315
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-04-17 20:49:21 -0400

    Added Mux to PC

--- PC.v
+++ PC.v
@@ -24,3 +24,18 @@ module ProgramCounter(PC, PC4, in, PS, clock, reset);
 	RegisterNbit PC_Reg (PC, PC_in, load_PC, reset, clock);
 	defparam PC_Reg.N = 64;
 endmodule
+
+module Mux4to1nbit (F, S, I00, I01, I02, I03);
+Parameter N = 4;
+input [1:0] S;
+input [n-1:0] I00, I01, I02, I03;
+output reg [N-1:0] F;
+always @(*) begin
+ case (S)
+   2'b00: F <= I00;
+   2'b01: F <= I01;
+   2'b10: F <= I02;
+   2'b11: F <= I03;
+ endcase 
+end
+endmodule
