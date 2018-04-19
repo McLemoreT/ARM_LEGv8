d7e779b2eac0159c27ca015239e18459f6d9c3ae
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-04-19 09:40:38 -0400

    Changed 'Mux4to1Nbit' to 'Mux4to1nbit'

--- PC.v
+++ PC.v
@@ -19,7 +19,7 @@ module ProgramCounter(PC, PC4, in, PS, clock, reset);
 	assign load_PC = 1'b1;
 	assign PC4 = PC + 4; // +4 adder
 	assign AdderOut = PC4 + {in[63:1], 2'b00};
-	Mux4to1Nbit PC_Mux (PC_in, PS, PC, PC4, in, AdderOut);
+	Mux4to1nbit PC_Mux (PC_in, PS, PC, PC4, in, AdderOut);
 	defparam PC_Mux.N = 64;
 	RegisterNbit PC_Reg (PC, PC_in, load_PC, reset, clock);
 	defparam PC_Reg.N = 64;
