67555d93a4bc5d2a8b6ef406790ac4faa47b09bf
sheppardd1 <35700049+sheppardd1@users.noreply.github.com>
2018-04-25 17:55:37 -0400

    IT WORKS!!!!!!!!

--- DatapathLEGv8.v
+++ DatapathLEGv8.v
@@ -1,9 +1,13 @@
-module DatapathLEGv8(ControlWord, status, data, clock, reset);
-	input [96:0] ControlWord;
-	inout [63:0] data;
+
+module DatapathLEGv8(/*ControlWord, status, data,*/ clock, reset);
+//	input [96:0] ControlWord;
+//	inout [63:0] data;
 	input clock, reset;
-	output [4:0] status;
+//	output [4:0] status;
 	
+	wire [4:0] status;
+	wire [63:0] data;
+	wire [96:0] ControlWord;
 	wire [63:0]address;
 	wire EN_B;	
 	wire [63:0] PC4;
@@ -17,19 +21,19 @@ module DatapathLEGv8(ControlWord, status, data, clock, reset);
 	wire [3:0] ALU_Status;
 	wire [1:0] PS;
 	wire EN_PC, SL, WM, WR; 
+	wire [63:0] A;
+	wire [63:0] constant2;
+	wire [31:0] instruction;
 	
-	assign {EN_PC, EN_Mem, EN_ALU, PCsel, Bsel, SL, WM, WR, PS, FS, SB, SA, DA} = ControlWord [28:0]; //we have the control word, pull all these things off of it in the correct order
+	assign {NS, constant, EN_PC, EN_Mem, EN_ALU, PCsel, Bsel, SL, WM, WR, PS, FS, SB, SA, DA, EN_B} = ControlWord; //we have the control word, pull all these things off of it in the correct order
 													    //Removed NS so CW went from 94 bits to 93
-
-	assign data = (EN_PC) ? PC4 : 1'bz;
-		
-	RegFile32x64 regfile(A, B, data, DA, SA, SB, WR, reset, clock);
-	
 	assign RegAbus = PCsel ? constant : A;
 	assign RegBbus = Bsel ? constant : B;
-	assign data = EN_B ? Bsel : 64'bz;
-	
-	ALU_LEGv8 alu (A, RegBbus, FS, FS[0], ALU_output, ALU_Status);
+														 
+	RegFile32x64 regfile(A, B, data, DA, SA, SB, WR, reset, clock);
+
+	ALU_LEGv8 alu (A, RegBbus, FS, FS[1], ALU_output, ALU_Status);
+
 	
 	RegisterNbit statusReg (status[4:1], ALU_Status, SL, reset, clock); //SL is part of control work. need to figure out how to get that here
 	defparam statusReg.N = 4;
@@ -42,19 +46,21 @@ module DatapathLEGv8(ControlWord, status, data, clock, reset);
 	
 	//RAM256x64 data_mem (ALU_output, ~clock, RegBbus, MemWrite, MEM_output); 
 		//clock is inverted so ALU operations happen before reading values. Gives time for fan-in time
-	RAM256x64 data_mem (ALU_output, ~clock, B, WM, MEM_output);
+	RAM256x64 data_mem (ALU_output, ~clock, data, WM, MEM_output);
 
 	//defparam data_mem.memory_words = 7000;
-	
+	assign data = (EN_PC) ? PC4 : 64'bz;
 	assign data = EN_Mem ? MEM_output : 64'bz;
 	assign data = EN_ALU ? ALU_output : 64'bz;
+
+	assign data = EN_B ? B : 64'bz;
 	
 	ProgramCounter PC (address, PC4, RegAbus, PS, clock, reset);
 	
 	
 	rom_case ROM (instruction, address[17:2]);
     
-	control_unit_setup c1 (instruction, status, reset, clock, control_word, constant)
+	control_unit_setup c1 (instruction, status, reset, clock, ControlWord, constant2);
 
 	
 endmodule
